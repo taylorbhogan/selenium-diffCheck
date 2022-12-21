4af5a8a0bb4a42c304713748a3d9662b65abd6c9
Taylor Hogan <taylorb.hogan@gmail.com>
2022-12-21 12:58:34 -0800

    Readability

--- __init__.py
+++ __init__.py
@@ -1,38 +1,29 @@
 import os
 import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
 
 def executeDiffChecking():
-	def handleFailure(driver, file_name, sheet_index=0):
-		print("start handleFailure")
-		screenshot_path = f'./screenshots/{time.time_ns()}-{os.path.splitext(file_name)[0]}-{sheet_index}.png'
-		result = driver.get_screenshot_as_file(screenshot_path)
+	def handleFailure(driver, file_name, sheet_index, error_string=""):
+		driver.get_screenshot_as_file(f'./screenshots/{time.time_ns()}-{os.path.splitext(file_name)[0]}-sheetidx:{sheet_index}{error_string}.png')
 
 	def handleSuccess():
 		print("match")
 
-	def findDifference(driver, file_name):
-		try:
-			submitButton = WebDriverWait(driver, 15).until(
-				EC.presence_of_element_located((By.NAME, "Find difference"))
-			)
-
-			submitButton.click()
-		except:
-			print("couldn't find the submit button")
-			driver.quit()
+	def findDifference(driver, file_name, sheet_index=0):
+		submitButton = WebDriverWait(driver, 15).until(
+			EC.presence_of_element_located((By.NAME, "Find difference"))
+		)
+		submitButton.click()
 
 		try:
 			driver.switch_to.alert.accept()
 			handleSuccess()
 		except:
-			handleFailure(driver, file_name)
+			handleFailure(driver, file_name, sheet_index)
+
 
 
 	driver = webdriver.Chrome()
@@ -48,33 +39,33 @@ def executeDiffChecking():
 		inputs = WebDriverWait(driver, 4).until(
 			EC.presence_of_all_elements_located((By.XPATH, "//input[@class='diff-input-header_fileInput__6v6Mq']"))
 		)
+		# load Excel docs into inputs
+		inputs[0].send_keys(f'{os.fsdecode(originalDirectoryPath)}/{os.fsdecode(originalExcelDocuments[idx])}')
+		inputs[1].send_keys(f'{os.fsdecode(changedDirectoryPath)}/{os.fsdecode(changedExcelDocuments[idx])}')
 
-		originalPath = f'{os.fsdecode(originalDirectoryPath)}/{os.fsdecode(originalExcelDocuments[idx])}'
-		changedPath = f'{os.fsdecode(changedDirectoryPath)}/{os.fsdecode(changedExcelDocuments[idx])}'
-
-		inputs[0].send_keys(originalPath)
-		inputs[1].send_keys(changedPath)
-
-		findDifference(driver, os.fsdecode(changedExcelDocuments[idx]))
+		findDifference(driver, os.fsdecode(changedExcelDocuments[idx])) #first (idx 0) sheet only
 
 		sheetNameSelects = WebDriverWait(driver, 15).until(
 			EC.presence_of_all_elements_located((By.XPATH, "//select[@class='excel-input_sheetSelect__p7MmN diffResult']"))
 		)
 		sheet_idx = 1
-		options = sheetNameSelects[0].find_elements(By.TAG_NAME, "option")
-		while sheet_idx < len(options):  #each sheet after the first
-			sheetNameSelects[0].click()
-			options[sheet_idx].click()
+		optionsLeft = sheetNameSelects[0].find_elements(By.TAG_NAME, "option")
+		optionsRight = sheetNameSelects[1].find_elements(By.TAG_NAME, "option")
+		if len(optionsLeft) == len(optionsRight):
+			while sheet_idx < len(optionsLeft):  #each sheet after the first
+				sheetNameSelects[0].click()
+				optionsLeft[sheet_idx].click()
 
-			sheetNameSelects[1].click()
-			optionsRight = sheetNameSelects[1].find_elements(By.TAG_NAME, "option")
-			optionsRight[sheet_idx].click()
+				sheetNameSelects[1].click()
+				optionsRight[sheet_idx].click()
 
-			findDifference(driver, os.fsdecode(changedExcelDocuments[idx]))
+				findDifference(driver, os.fsdecode(changedExcelDocuments[idx]), sheet_idx)
 
-			sheet_idx += 1
+				sheet_idx += 1
 
-		print("iteration complete")
+			print("iteration complete")
+		else:
+			handleFailure(driver, os.fsdecode(changedExcelDocuments[idx]), sheet_idx, "NUM SHEETS MISMATCH")
 
 	print("script finished executing")
 
